c3121e8274613841263288560c166c989b41afd5
TJMKuijpers <tim.kuijpers@maastrichtuniversity.nl>
2019-12-04 20:19:48 +0100

    update

--- createBarPlot.js
+++ createBarPlot.js
@@ -1,9 +1,9 @@
 function createBarPlot(data_for_bar_plot){
     // chart.js barplot   
     var ctx = document.getElementById('barChart').getContext('2d');
-var barchart  = new CharacterData(ctx, {
+var barchart  = new Chart(ctx, {
     type: 'bar',
-    data:data_for_bar_plot,
+    data:[12, 19, 3, 5, 2, 3],
     options: options = {
         scales: {
             xAxes: [{
